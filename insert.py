dd283194f3afc25c8aca6b5b48bfae7aa0cd1ba0
Minh Trang <minhtranglamthi@gmail.com>
2014-10-29 18:08:10 +0800

    insert and fetch from sql server

---
+++ insert.py
@@ -0,0 +1,19 @@
+import MySQLdb
+
+db = MySQLdb.connect("localhost","root","ezfx0109","TESTDB")
+cursor = db.cursor()
+
+# Prepare SQL query to INSERT a record into the database.
+sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+         LAST_NAME, AGE, SEX, INCOME)
+         VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+
+db.close()
--- webCrawler.py
+++ webCrawler.py
@@ -1,23 +1,20 @@
 import urllib
 import urllib2
 import re
+import MySQLdb
 
 patterns ={'http://www.travelex.com.my/MY/For-Individuals/Foreign-Exchange-Rates/Today-s-Online-Rates/': '(var rates=.*)'}
 
-def Connect2Web():
-	url = 'http://www.travelex.com.my/MY/For-Individuals/Foreign-Exchange-Rates/Today-s-Online-Rates/'
+def Connect2Web(url, pattern):
 	aResp = urllib2.urlopen(url)
 	web_pg = aResp.read()
-	rates = re.search(patterns[url], web_pg).group(1)
+	rates = re.search(pattern, web_pg).group(1)
 	rates = findRates(rates)
-	
-	
-	return str(rates)
+	return rates
 
 #input: string of html text contains rates
 #output: dictionary in form of {'Currency Code': rates}
 def findRates(webtext):
-
 	result = {}
 	i = 0
 	while i != -1:
@@ -26,14 +23,14 @@ def findRates(webtext):
 		ExRate, i = getContent(webtext, 'ExchangeRate":', i)
 		if i == -1 :
 			break
-		result[CcyCode] = ExRate
+		result[CcyCode] = ExRate[:-1]
 	
 	return result
 
-#input: string to search, index of string to begin search, the patterns
+#input: string to search, start patern, end pattern, starting index
 #output: desired text in the content and the index at the end of content
-def getContent(webtext, patterns, start):
-	start = webtext.find(patterns, start)
+def getContent(webtext, SP, EP, start):
+	start = webtext.find(SP, start)
 	content = -1
 	end = -1
 
@@ -45,6 +42,57 @@ def getContent(webtext, patterns, start):
 	#return -1 if not found
 	return content, end
 
-textfile = file('cache.txt','wt')
-textfile.write(Connect2Web())
-textfile.close()
+#textfile = file('cache.txt','wt')
+#textfile.write(Connect2Web())
+#textfile.close()
+
+#generate unique ID for each rate - FORMAT: DDMMYY-url-FCYLCY
+def generateID(day, url, LCY, FCY):
+	return day+'-'+url+'-'+FCY+LCY
+
+#input: website name
+#outout: ID, url, patterns
+def getDetails(name):
+	db = MySQLdb.connect("localhost","root","ezfx0109","crawlerdb")
+	cursor = db.cursor()
+	# Prepare SQL query to INSERT a record into the database.
+	sql = "SELECT * FROM url \
+	       WHERE name = '%s'" % (name)
+	try:
+		cursor.execute(sql)
+		results = cursor.fetchone()
+		return results[1],results[2],results[4]
+	except:
+	   print "Error: unable to fecth data"
+	db.close()
+
+def insert(url, pattern):
+	db = MySQLdb.connect("localhost","root","ezfx0109","crawlerdb")
+	cursor = db.cursor()
+	#Prepare SQL query to INSERT a record into the database.
+	rates = Connect2Web(url, pattern)
+	vals = []
+	for e in rates:
+		sellCCY = e
+		offer = rates[e]
+		vals.append((sellCCY,offer))
+
+	sql = "INSERT INTO RATES (SELLCCY, OFFER)\
+			VALUES (%s, %s)"
+	try:
+		# Execute the SQL command
+		cursor.executemany(sql,vals)
+		# Commit your changes in the database
+		db.commit()
+	except:
+		# Rollback in case there is any error
+		db.rollback()
+	db.close()
+
+def main():
+	target = raw_input('Enter the name of target website: ')
+	ID, url, pattern = getDetails(target)
+	print pattern
+	insert(url, pattern)
+
+main()
