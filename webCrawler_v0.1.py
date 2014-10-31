f82365ab514de99ade93366dca2d0be07acd51df
Minh Trang <minhtranglamthi@gmail.com>
2014-10-31 11:51:16 +0800

    fix bugs for Money Master website

--- webCrawler_v0.1.py
+++ webCrawler_v0.1.py
@@ -5,29 +5,28 @@ import MySQLdb
 import time
 from time import strftime
 
-def Connect2Web(url, patterns):
+def Connect2Web(url, patterns, order):
 	aResp = urllib2.urlopen(url)
 	web_pg = aResp.read()
 
 	mainPattern = patterns[0]
 	rates = web_pg[web_pg.find(mainPattern):]
-	rates = rates.replace('\n','')
-	rates = re.sub(' +',' ', rates)
-	#rates = finding(rates, patterns)
+	rates = re.sub(r"\s+", " ", rates)
+	rates = finding(rates, patterns, order)
 
 	return rates
 
 #input: string of html text contains rates, start patern, end pattern
 #output: dictionary in form of [CurrencyCode, unit, bid, offer, date]
-def finding(webtext,patterns):
+def finding(webtext, patterns, order):
 	result = []
 	i = 0 #starting position
 	while i != -1:
 		output = []
-		for j in range(1,7):
-			#find the buyCCY, sellCCY, bid, offer, date and unit (6)
-			SP = patterns[j]
-			EP = patterns[j+6]
+		for j in range(0,6):
+			#find the buyCCY, sellCCY, bid, offer, date and unit (6) according to order
+			SP = patterns[order[j]+1]
+			EP = patterns[order[j]+7]
 			content = ""
 			#if it does not have end pattern, the website does not provide such the information
 			if EP != "":
@@ -38,7 +37,10 @@ def finding(webtext,patterns):
 			if content == -1:
 				break
 			output.append(content)
+
 		if output:
+			#re-order before adding to result
+			output = reOrder(output, order)
 			result.append(output)
 	
 	return result
@@ -54,9 +56,28 @@ def getContent(webtext, SP, EP, start):
 		start += len(SP)
 		end = webtext.find(EP,start)
 		content = webtext[start:end]
+		end += 1
 
-	#return -1, -1 if not found
+	#return -1, -1 if not found, end + 1 to avoid capturing the wrong text when the sign to recognize is not unique
 	return content, end
+
+#reorganize the output to follow the standard order [buyCCY, sellCCY, bid, offer, date, unit]
+#input: the list and the current order
+#output: list follow the standard order [0,1,2,3,4,5]
+def reOrder(lst, order):
+	i = 0
+	temp = [] + order #to avoid the order change after going through process
+	while i < len(temp):
+		if i != temp[i]:
+			#find current position of the right item for position i
+			pos = temp.index(i)
+			#swap it with the item in the position i
+			lst[i], lst[pos] = lst[pos], lst[i]
+			#update the order
+			temp[i], temp[pos] = temp[pos], temp[i]
+		i += 1
+
+	return lst
 
 #textfile = file('cache.txt','wt')
 #textfile.write(Connect2Web())
@@ -92,17 +113,29 @@ def insert(urlCode_url, patterns):
 	url = urlCode_url[1]
 	# standard order [0,1,2,3,4,5] = [buyCCY, sellCCY, bid, offer, date, unit]
 	order = convertToList(patterns[13])
-	data = Connect2Web(url, patterns)
-	print data
-	'''
+	data = Connect2Web(url, patterns, order)
+
 	vals = []
+	
 	for e in data:
 		#[buyCCY, sellCCY, bid, offer, date, unit]
-		date = time.strptime(e[order[4]],'%A, %d %b %Y %H:%M:%S')
+		date = e[4]
+		while True:
+			try:
+				date = time.strptime(date,'%A, %d %b %Y %H:%M:%S')
+				break
+			except ValueError:
+				try:
+					date = time.strptime(date, '%m/%d/%Y at %H:%M %p')
+					break
+				except ValueError:
+					print "Date Time format does not matched our stored format. Please review and update!"
+					break
 		date = strftime('%d%m%y', date)
-		buyCCY = e[order[0]]
-		sellCCY = e[order[1]]
-		ID = generateID(date, urlCode, buyCCY, sellCCY)
+
+		buyCCY = e[0]
+		sellCCY = e[1]
+		ID = generateID(date, urlCode, sellCCY, buyCCY)
 		row = [ID, urlCode]
 		for entry in e:
 			row.append(entry)
@@ -120,7 +153,7 @@ def insert(urlCode_url, patterns):
 	except:
 		# Rollback in case there is any error
 		db.rollback()
-	'''
+
 	db.close()
 
 #find the order of the website data, put it into list for taking out correctly in insert()
